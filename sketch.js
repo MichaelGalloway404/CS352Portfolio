31ea2ce47ed771d25a8855dd4c2831ae01f5d342
Michael Galloway <michael.d.galloway404@gmail.com>
2025-04-15 12:21:27 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    let canvas = createCanvas(400, 400);
+    canvas.parent('sketch-holder'); // Attach the canvas to a specific div
+    background(220);
+    
+    // make a blue circle
+    fill(100, 100, 255);
+    ellipse(200, 200, 40, 40);
+  }
+  
+  function draw() {
+  }
