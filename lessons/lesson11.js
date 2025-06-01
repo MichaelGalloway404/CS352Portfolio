fda9e8356ae37b6c3830ebc862d84e28ffa2beb4
Michael Galloway <michael.d.galloway404@gmail.com>
2025-06-01 11:18:36 -0500

    Add files via upload

--- lessons/lesson11.js
+++ lessons/lesson11.js
@@ -53,13 +53,13 @@ function draw() {
     // let cropped = myVideo.get(100, 100, 200, 200);
     //   image(cropped, 0, 0);
 
-    // loadPixels();
-    // for (let i = 0; i < pixels.length; i += 4) {
-    //     pixels[i] = 255 - pixels[i];         // Invert red channel
-    //     pixels[i + 1] = 255 - pixels[i + 1]; // Invert green
-    //     pixels[i + 2] = 255 - pixels[i + 2]; // Invert blue
-    // }
-    // updatePixels();
+    loadPixels();
+    for (let i = 0; i < pixels.length; i += 4) {
+        pixels[i] = 255 - pixels[i];         // Invert red channel
+        pixels[i + 1] = 255 - pixels[i + 1]; // Invert green
+        pixels[i + 2] = 255 - pixels[i + 2]; // Invert blue
+    }
+    updatePixels();
 
 
     // Useful p5.MediaElement Methods
